bc46ed54f4ea7cd3fa07b1ba390bb268e978aeb7
gd0minykas <gdominykasc@gmail.com>
2024-10-14 21:21:29 +0300

    logos added, register begun

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,28 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FacebookLogo from "@/components/logos/FacebookLogo.vue";
+import GoogleLogo from "@/components/logos/GoogleLogo.vue";
+</script>
 
 <template>
   <div class="container">
     <div class="row">
       <div class="col align-self-start">
-        <h1>BooksInn</h1>
+        <h1 class="fs-1">BooksInn</h1>
         <h2>Register as a new user</h2>
       </div>
       <div class="col">
         <form>
-          <div class="mb-2">
+          <div class="mb-3">
             <label for="email" class="form-label">Email:</label>
             <input type="email" id="email" class="form-control" />
           </div>
-          <div class="mb-2">
-            <label for="username" class="form-label">Username:</label>
-            <input type="text" id="username" class="form-control" />
-          </div>
-          <div class="mb-5">
+          <div class="mb-3">
             <label for="password" class="form-label">Password:</label>
             <input type="password" id="password" class="form-control" />
           </div>
-          <div class="mb-3">
-            <!-- providers -->
+          <div class="mb-4">
+            <label for="password" class="form-label">Repeat Password:</label>
+            <input type="password" id="password" class="form-control" />
+          </div>
+          <div class="mb-3 d-flex flex-row-reverse">
+            <button type="button" class="ms-5 btn btn-warning">Register</button>
+            <a href="#"><FacebookLogo class="mx-3" /></a>
+            <a href="#"><GoogleLogo class="me-3" /></a>
           </div>
         </form>
       </div>
